2a97b7e2f30ddc895449244227b1d8c28d66e49a
OUCliuxiang <xnxdge@163.com>
2021-09-14 14:31:35 +0800

    14thSep2021

--- StudyPlan/Algo1/day1/278_First_Bad_Version.cpp
+++ StudyPlan/Algo1/day1/278_First_Bad_Version.cpp
@@ -60,4 +60,18 @@ public:
         }
         return 0;
     }
+
+    int firstBatVersion_offical(int n){
+        int left = 1;
+        int right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
 };
