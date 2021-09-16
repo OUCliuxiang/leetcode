a5b0a7b5f77239790f2437b178b794e35fdee978
OUCliuxiang <xnxdge@163.com>
2021-09-16 15:53:50 +0800

    16thSep2021

--- StudyPlan/Algo1/day4/557_Reverse_Words_in_a_String_III.cpp
+++ StudyPlan/Algo1/day4/557_Reverse_Words_in_a_String_III.cpp
@@ -1,11 +1,34 @@
+/* 
+ * Given a string s, reverse the order of characters in each word 
+ * within a sentence while still preserving whitespace and initial 
+ * word order.
+ * 
+ * Example 1:
+ * Input: s = "Let's take LeetCode contest"
+ * Output: "s'teL ekat edoCteeL tsetnoc"
+ * 
+ * Example 2:
+ * Input: s = "God Ding"
+ * Output: "doG gniD"
+ * 
+ * Constraints:
+ * 1 <= s.length <= 5 * 104
+ * s contains printable ASCII characters.
+ * s does not contain any leading or trailing spaces.
+ * There is at least one word in s.
+ * All the words in s are separated by a single space.
+ */ 
+
 class Solution {
 public:
     string reverseWords(string s) {
-        int found = 0;
-        while (found != -1){
-            int wordeEnd = s.find_first_of(' ', found + 1);
-            reverse(s.begin()+found, s.begin()+wordEnd);
-            found = wordEnd();
-        } 
+        int found = -1;
+        while (found != s.length()){
+            int wordEnd = s.find_first_of(' ', found + 1);
+            wordEnd = (wordEnd == -1 ? s.length() : wordEnd);
+            reverse(s.begin()+found+1, s.begin()+wordEnd);
+            found = wordEnd;
+        }
+        return s;
     }
 };
