e477d1bc4dfcc4d9b9ed03b5c7441cda48e1af67
OUCliuxiang <xnxdge@163.com>
2021-09-18 22:08:07 +0800

    18thSep2021

--- StudyPlan/Algo1/day6/3_Longest_Substring_Without_Repeating_Characters.cpp
+++ StudyPlan/Algo1/day6/3_Longest_Substring_Without_Repeating_Characters.cpp
@@ -11,4 +11,18 @@ public:
         }
         return maxLen;
     }
+
+    int lengthOfLongestSubstring_Modified(string s) {
+        map<char, int> charMap;
+        int start = -1, maxLen = 0;
+        for(int i = 0; i < s.length(); i++){
+            if (charMap.count(s[i]) != 0)
+                start = max(charMap[s[i]], start);
+                // 当前元素上一次出现的位置与上一组重复元素 
+                // 倒数第二次出现的位置做个比较
+            charMap[s[i]] = i;
+            maxLen = max(maxLen, i-start);
+        }
+        return maxLen;
+    }
 };
