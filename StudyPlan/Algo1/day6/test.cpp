73d93797ea6bd73606d74f9ee27d6079081ba7b7
OUCliuxiang <xnxdge@163.com>
2021-09-19 19:12:54 +0800

    19thSep2021

--- StudyPlan/Algo1/day6/test.cpp
+++ StudyPlan/Algo1/day6/test.cpp
@@ -1,24 +1,38 @@
-#include <cstdio>
-#include <string>
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
 
-int main(){
-    std::string s = "abcabcbb";
-    int res = 0;
-    std::string sub = "";
-    for (int i = 0; i < s.length(); i++){
-        printf("%d",i);
-        if (i == s.length()-1)
-            break;
-        int len = res;
-        while (sub.find(s[i]) == -1){
-            sub += s[i++];
-            len ++;
-            res = (len > res ? len : res);
-            // printf("%s",sub);
-        }                        
-        i = i + 1 - sub.length() + sub.find(s[i]);
-        sub = s.substr(i, len-sub.find(s[i]));
-        
+using namespace std;
+
+class Solution{
+public:
+    bool checkInclusion(string s1, string s2) {
+        vector<string> permutations;
+        sort(s1.begin(), s1.end());
+        do{
+            permutations.push_back(s1);
+        }while(next_permutation(s1.begin(), s1.end()));
+        for (vector<string>::iterator it = permutations.begin(); it != permutations.end(); it++){
+            printf("%s\n", (*it).c_str());
+            if (s2.find(*it)){
+                return true;
+            }
+        }
+        return false;
     }
-    return 0;
+};
+
+int main()
+{
+    Solution solu;
+	string s1 = "ab", s2 = "eidboaoo";
+    bool flag = solu.checkInclusion(s1, s2);
+    if (flag){
+        printf("true\n");
+    }else{
+        printf("false\n");
+    }
+    int inde = s2.find(s1);
+    cout << inde << endl;
 }
